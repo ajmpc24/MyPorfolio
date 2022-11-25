8a3b3939611972e29ebb0680a907ef77d821067a
ajmpc24 <ajmpc24@gmail.com>
2022-11-24 21:26:27 -0300

    Agregando Loader al porfolio - html, css, js

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	document.getElementById('container').style.display = 'none';
+	document.getElementById('content').style.display = 'block';
+};
+$(window).load(function () {
+	$('#loading').fadeOut();
+	$('#loading-img').delay(150).fadeOut('slow');
+}),
+	4000;
